316b74d89756c5bfa2b31daa7aae75a6ca9b00d0
Bishal487 <bishal.kunwar487@gmail.com>
2023-12-09 12:16:40 -0500

    Added SumExp ProductExp and ModuloExp, the ArithmeticOperation is the main java file to run the program and the test case

--- src/ArithmeticOperation.java
+++ src/ArithmeticOperation.java
@@ -1,76 +1,21 @@
 
-// creating abstract class the ArithmeticExp
-abstract class ArithmeticExp{
-
-    // evaluation int method to abstract:
-    public abstract int evaluate();
-
-    //  toString String method to abstract
-    public abstract String toString();
-}
-
-
-// Class that represents the number in the expression
-class NumberExp extends ArithmeticExp {
-    private int value;
-
-    public NumberExp(int value) {
-        this.value = value;
-    }
-
-    @Override
-    public int evaluate() {
-        return value;
-    }
-
-    @Override
-    public String toString() {
-        return Integer.toString(value);
-    }
-}
-
-
-// Abstract binary class for binary expressions (Sum, Product, Modulo)
-abstract class BinaryExp extends ArithmeticExp {
-    protected ArithmeticExp left;
-    protected ArithmeticExp right;
-    protected String operator;
-
-    public BinaryExp(ArithmeticExp left, ArithmeticExp right, String operator) {
-        this.left = left;
-        this.right = right;
-        this.operator = operator;
-    }
-
-    @Override
-    public String toString() {
-        return "(" + left.toString() + " " + operator + " " + right.toString() + ")";
-    }
-}
-
-
-
-// Main class to run the whole program of ArithmeticOperation
+// Main class ArithmeticOperation to run the whole program of ArithmeticOperation
 public class ArithmeticOperation {
     public static void main(String[] args) {
 
+        // creating the test 3 + 2 * 5
+        // variables are in reference to the test case
+        ArithmeticExp term = new SumExp(
+                new NumberExp(3),
+                new ProductExp(new NumberExp(2),
+                        new NumberExp(5)
+                )
+        );
+
+        // Testing the program result from the expression and then evaluating the result
+        System.out.println("" + " Let's Suppose The Expression: " + term.toString());
+        System.out.println("Considering The Expression the Evaluation Result would be: " + term.evaluate());
+
 
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
